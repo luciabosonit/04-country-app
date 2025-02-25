0f47e3ac55b43140d574fe1ced7dd786d7b0eaa2
Lucía Cruz Jurado <lucia.cruz@bosonit.com>
2025-02-25 13:41:23 +0100

    Finalización del vídeo CountryService - Interfaces

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CountrySearchInputComponent } from "../../components/country-search-input/country-search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -9,8 +10,10 @@ import { CountryListComponent } from "../../components/country-list/country-list
 })
 export class ByCapitalPageComponent {
 
-  onSearchFather(textCapital: String){
-    alert("Mensaje recibido: " + textCapital);
+  countryService = inject(CountryService);
+
+  onSearchFather(textCapital: string){
+    this.countryService.searchByCapital(textCapital).subscribe((resp) => {console.log(resp)});
   }
 
  }
